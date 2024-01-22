f1b47c19000125675d1a7875e7975b907fa7a3c0
Remofox <Remo.fox@gmail.com>
2024-01-22 19:29:08 +0200

    y-t

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navBar = document.querySelector('.navbar');
+let Da = document.querySelector('#d-a')
+let RSE = document.querySelector('.right-section')
+function rr(){
+    menuBtn.classList.toggle('fa-times');
+    navBar.classList.toggle('i'); 
+}
+function ud(){
+    Da.classList.toggle('ri-arrow-up-s-line');
+    RSE.classList.toggle('rs');
+}
